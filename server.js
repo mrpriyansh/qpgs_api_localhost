b293718189bf3cba86f7cbecb86ae6f6f351b45f
Priyansh <mr.priyanshgaharana@gmail.com>
2019-12-09 16:46:17 +0530

    heroku

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const db=knex({
 
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'Priyansh',
-    password : '123',
-    database : 'qpgs'
+    connectionString : process.env.DATABASE_URL,
+    ssl:true,
   }
 });
 /*
